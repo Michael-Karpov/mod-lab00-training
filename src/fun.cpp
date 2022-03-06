57df0a3ed6f4fd70741aadd26a650875c76d6b57
Mihail Karpov <100800994+Michael-Karpov@users.noreply.github.com>
2022-03-06 12:29:07 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,10 +2,9 @@
 #include "fun.h"
 
 int64_t power(int64_t x, uint16_t n) {
-    int64_t z= x;
-    for (int i = 0; i < n-1; i++)
-    {
-        x*=z;
+    int64_t z = x;
+    for (int i = 0; i < n - 1; i++) {
+        x *= z;
     }
     return x;
 }
